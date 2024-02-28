c28dace826d3a25d44562852f5bd7a106debb4f9
Muhinde234 <igirimpuhwedosta@gmail.com>
2024-02-28 13:39:36 +0200

    first commit

--- user_input.py
+++ user_input.py
@@ -3,6 +3,7 @@ age=input("please enter your age : ")
 location=input("enter where you live : ")
 a= int(age)
 
+
 print("hello " + name)
 print("you are " +  str(a))
 print("you live  in "+ location)
